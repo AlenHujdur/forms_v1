b53b70054bcac06e4d6a45cd3f92d7311dc567b1
Alen Hujdur <hujduralen@gmail.com>
2016-11-06 20:48:28 +0100

    video player added

--- WindowsFormsApplication1/WindowsFormsApplication1/Prodaja/Prodaja/Form3.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Prodaja/Prodaja/Form3.Designer.cs
@@ -28,19 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.lstMovies = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(1, 0);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(497, 363);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            // 
+            // lstMovies
+            // 
+            this.lstMovies.FormattingEnabled = true;
+            this.lstMovies.Location = new System.Drawing.Point(505, 13);
+            this.lstMovies.Name = "lstMovies";
+            this.lstMovies.Size = new System.Drawing.Size(245, 303);
+            this.lstMovies.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(505, 323);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(245, 29);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Close Application";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(762, 364);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lstMovies);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
             this.Name = "Form3";
             this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.ListBox lstMovies;
+        private System.Windows.Forms.Button button1;
     }
 }
--- WindowsFormsApplication1/WindowsFormsApplication1/Prodaja/Prodaja/Form3.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Prodaja/Prodaja/Form3.cs
@@ -7,14 +7,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Prodaja
 {
     public partial class Form3 : Form
     {
+        SqlConnection cn;
+        SqlCommand cmd;
         public Form3()
         {
             InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            string cs = "Data Source = DESKTOP-97OEARS\\SQLEXPRESS; Initial Catalog = PSDB; Integrated Security = True";
+            cn = new SqlConnection(cs);
+            cmd = new SqlCommand();
+            cmd.Connection = cn;
+
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "GetVideo";
+            cn.Open();
+            SqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+            lstMovies.DataSource = dt;
+            lstMovies.DisplayMember = "vtitle";
+            lstMovies.ValueMember = "id";
+    
         }
     }
 }
