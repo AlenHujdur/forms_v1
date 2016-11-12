18f1156bd18ca1472ad43b9a6aeef137c5b78bcc
Alen Hujdur <hujduralen@gmail.com>
2016-11-12 21:42:52 +0100

    sa Zukom

---
+++ Pretplatnici/PretplatniciDA/Pretplatnici.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PretplatniciDA.DATA;
+
+namespace PretplatniciDA
+{
+    public class Pretplatnici
+    {
+        public static void InsertPretplatnik(Pretplatnici p)
+        { 
+            using(PretplatniciEntities dm = new PretplatniciEntities())
+	        {
+                dm.usp_PretplatniciInsert(p.,p.)
+	        }
+
+        }
+    }
+}
